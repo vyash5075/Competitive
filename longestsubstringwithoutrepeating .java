c99493e86de1edd3a69eec3a61f90a8ee9f8f2da
vyash5075 <vyash5075@gmail.com>
2021-03-24 10:18:27 +0530

    longest unique sibstring

--- longestsubstringwithoutrepeating .java
+++ longestsubstringwithoutrepeating .java
@@ -2,30 +2,29 @@
 package competitive;
 import java.util.*;
 public class longestsubstringwithoutrepeating {
+     public static  int lengthOfLongestSubstring(String s) {      
+       int i=0;
+        int j=0;
+        int max=0;
+        HashSet<Character>hs=new HashSet();
+        while(j<s.length()){
+            if(!hs.contains(s.charAt(j))){
+                hs.add(s.charAt(j));
+                j++;
+                
+                max=Math.max(hs.size(),max);
+            }
+            else{
+                hs.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return max;
+    }    
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
-        ArrayList<Character>al=new ArrayList<Character>();
-        int count=0;
-        for(int i=0;i<str.length();i++){
-            
-            if(al.contains(str.charAt(i))){
-                 if(al.size()>count){
-                    count=al.size();
-                }
-              al.removeAll(al);
-               al.add(str.charAt(i));
-             
-            }
-            else{ 
-                
-                al.add(str.charAt(i));
-                if(al.size()>count){
-                    count=al.size();
-                }
-                 
-            }
-        }
-        System.out.println(count);
+       
+        System.out.println(lengthOfLongestSubstring(str));
     }
 }
